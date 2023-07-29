4b47091bd49aa48ca2f480bc5730295fbfec89c5
Dominic <lovric.domagoj1@gmail.com>
2023-07-29 11:10:15 +0100

    Story generation

--- app/app.py
+++ app/app.py
@@ -1,16 +1,33 @@
 import streamlit as st
+from story_generation.story_generator import StoryGenerator
 
 def main():
     st.title("Welcome to VoiceUp!")
     st.header("This is a platform for creators to create short clips based on their own content.")
 
-    # taking user inputs
-    story_summary = st.text_input("Enter a short summary for your story")
-    story_keywords = st.text_input("Enter up to 5 keywords for your story, separated by commas")
+    # Create an instance of StoryGenerator
+    generator = StoryGenerator()
 
-    if st.button("Generate Story"):
-        # replace the following line with the actual functions to generate story, voice and video
-        st.write("Story, voice and video will be generated here!")
+    # User can select to provide full story or generate it
+    story_option = st.selectbox("Choose an option:", ["I will provide the full story", "Generate story based on my summary"])
+
+    if story_option == "I will provide the full story":
+        user_story = st.text_area("Enter your story here:")
+        if st.button("Submit Story"):
+            st.text_area("Your Story:", value=user_story, max_chars=None)
+
+    else: # story_option == "Generate story based on my summary"
+        with st.form(key='my_form'):
+            story_summary = st.text_input("Enter a short summary for your story")
+            submit_button = st.form_submit_button(label='Generate Story')
+
+            if submit_button:
+                with st.spinner('Generating your story...'):
+                    # Generate story
+                    story = generator.generate_story(story_summary)
+                    print(story)
+                # TODO: Implement a clean output text method
+                st.text_area("AI Generated Story:", value=story.lstrip())
 
 if __name__ == "__main__":
     main()
---
+++ app/story_generation/story_generator.py
@@ -0,0 +1,74 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain, SimpleSequentialChain
+from langchain.memory import SimpleMemory
+
+import openai
+import os
+from dotenv import load_dotenv
+
+
+# Load the environment variables
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(current_dir))
+env_paht = os.path.join(project_root, ".env")
+load_dotenv(env_paht)
+
+
+class StoryTemplates:
+
+    story_template = """You are a storywriter. Given a short description, you can generate a story based on the idea in 75-100 words.
+    
+    Idea: {idea}"""
+
+    review_template = """You are a story critic. Given the generated story and the initial idea, it is your job to write a feedback on how to
+    imporve the story. Pay attention to things such as:
+    
+    1. Is the length of the story within 75-100 words?
+    2. Is the story engaging?
+    
+    Story: {story}"""
+
+    improve_template = """You are a storywriter. Given a generated story and a review from a critic, it is your job to improve the story.
+    Make sure you set the story length to MAXIMUM 150 words.
+    Story: {story}
+    Review: {review}
+    """
+
+
+class StoryGenerator:
+    def __init__(self, model: str = "gpt3.5-turbo"):
+
+        self.model = model
+        self.llm = OpenAI(temperature=0.9, openai_api_key=os.environ.get("OPENAI_KEY"))
+
+    
+    def generate_story(self, idea):
+
+        # Story generation
+        story_template = PromptTemplate(input_variables=["idea"], template=StoryTemplates.story_template)
+        story_chain = LLMChain(llm=self.llm, prompt=story_template, output_key="story")
+
+        # Review
+        review_template = PromptTemplate(input_variables=["story"], template=StoryTemplates.review_template)
+        review_chain = LLMChain(llm=self.llm, prompt=review_template, output_key="review")
+
+        # Improve
+        improve_template = PromptTemplate(input_variables=["story", "review"], template=StoryTemplates.improve_template)
+        improve_chain = LLMChain(llm=self.llm, prompt=improve_template)
+
+        final_chain = SequentialChain(
+            chains=[story_chain, review_chain, improve_chain],
+            input_variables=["idea"],
+            verbose=True
+        )
+
+        return final_chain.run(idea)
+
+
+if __name__ == "__main__":
+
+    chatbot = StoryGenerator()
+
+    # Testing
+    print(chatbot.generate_story("Story about a hackathon where a team of engineers is using elevenlabs to develop voice applications."))
